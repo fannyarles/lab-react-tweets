6101459342d400d051c3f6fff16f51f28fce70a1
fannyarles <fanny.arles@orange.fr>
2023-01-16 13:53:06 +0100

    done

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+function ProfileImage(props) {
+    return(
+        <img
+        src={ props.src }
+        className="profile"
+        alt="profile"
+        />
+    );
+}
+
+export default ProfileImage;
